e9d9465ace055b4472777a03567418ed935ee336
AaroneGeorge <aaroneofficial1879@gmail.com>
2025-01-08 14:46:12 +0530

    tool tip content modified

--- ui/app/create/page.tsx
+++ ui/app/create/page.tsx
@@ -270,7 +270,7 @@ export default function CreatePage() {
             <div>
               <label className="text-sm font-medium text-zinc-200 flex items-center">
                 Contract Address <span className="text-red-500">*</span>
-                <HoverTooltip content="Paste the contract address of the agent after deploying it as a token to arbitrum." />
+                <HoverTooltip content={"Paste the contract address of the agent after deploying it as a token to arbitrum. \nFor testing agents, you can give something by default."} />
               </label>
               <Input
                 required
