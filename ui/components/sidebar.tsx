f8c435e635c08a3c7672a4946cfd9fbd60f2a655
Nikhil PN <nikhilpn725@gmail.com>
2025-01-09 23:40:15 +0530

    sidemenu line added in sidebar

--- ui/components/sidebar.tsx
+++ ui/components/sidebar.tsx
@@ -40,7 +40,7 @@ export function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-zinc-950 text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-zinc-950 text-white border-r border-white/20">
       <div className="px-3 py-2 flex-1">
         <Link href="/" className="flex items-center pl-3 mb-5">
           {/* similar gif urls; */}
